a1cbb06c9ae629054d42a5a502c3fe594ff14398
ReiLaciste <raylaciste@yahoo.com>
2024-10-27 21:53:02 -0700

    add functionality for: listening for incoming connections, broadcasting messages, handle joining and leaving, concurrency

--- tcpccs.java
+++ tcpccs.java
@@ -2,28 +2,106 @@ import java.io.*;
 import java.net.*;
 
 class tcpccs {
+    private static final int PORT = 12345;
+    public static volatile boolean running = true;
     public static void main(String[] args) throws Exception {
-        
+
+        // Register a shutdown hook to catch Ctrl+C (SIGINT)
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("\nCtrl+C detected. Stopping the main thread...");
+            running = false; // This will break the main loop
+        }));
+
+        if (args.length != 2) {
+            System.out.println("Usage: java tcpccs <hostname> <username>");
+            System.exit(1);
+        }
+
+        String hostname = args[0];
+        String username = args[1];
+
+        // establishes connection to server
+        Socket clientSocket = new Socket(hostname, PORT);
+        System.out.println("Connected to the server. You can start sending messages.");
+
+        // Communication Streams
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
+        // Thread for listening for server messages
+        // Passes the clients connection and the server message
+        Thread serverMessageListenerThread = new Thread(new ServerMessageListener(clientSocket, inFromServer));
+        serverMessageListenerThread.start();
 
-        String sentence = inFromUser.readLine();
-        sendData = sentence.getBytes();
+        // Thread for sending messages to the server
+        Thread inputThread = new Thread(new clientMessageInput(clientSocket, inFromUser, outToServer, username));
+        inputThread.start();
+    }   
 
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 12345);
-        clientSocket.send(sendPacket);
+}
 
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
+class ServerMessageListener implements Runnable {
+    private Socket clientSocket;
+    private BufferedReader inFromServer;
 
-        String modifiedSentence = new String(receivePacket.getData());
+    public ServerMessageListener(Socket socket, BufferedReader inFromServer) {
+        this.clientSocket = socket;
+        this.inFromServer = inFromServer;
+    }
 
-        System.out.println("FROM SERVER: " + modifiedSentence);
+    @Override
+    public void run() {
+        try {
+            String serverMessage;
+            // Continuously listen for messages from the server
+            while ((serverMessage = inFromServer.readLine()) != null) {
+                System.out.println(serverMessage);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
 
-        clientSocket.close();
+class clientMessageInput implements Runnable {
+    private Socket clientSocket;
+    private BufferedReader inFromUser;
+    private DataOutputStream outToServer;
+    private String username;
+
+    public clientMessageInput(Socket socket, BufferedReader inFromUser, DataOutputStream outToServer, String username) {
+        this.clientSocket = socket;
+        this.inFromUser = inFromUser;
+        this.outToServer = outToServer;
+        this.username = username;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String clientMessage;
+            while (tcpccs.running) {
+                clientMessage = inFromUser.readLine();
+
+                if (clientMessage.equals("/quit")) {
+                    break;
+                }
+                String formattedMessage = username + ": " + clientMessage;
+                outToServer.writeBytes(formattedMessage + "\n");
+            }
+        } catch (IOException e) { 
+            System.out.println(e);
+        } finally {
+            try {
+                // Close the socket and streams when quitting
+                outToServer.close();
+                clientSocket.close();
+                inFromUser.close();
+                System.out.println("Disconnected from the server.");
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
     }
 }
--- tcpcss.java
+++ tcpcss.java
@@ -1,27 +1,115 @@
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 class tcpcss {
+    public static volatile boolean running = true;
+    private static final int PORT = 12345;
+    private static final ConcurrentHashMap<String, Client> clientList = new ConcurrentHashMap<>();
+    private static int numClient = 0;
     public static void main(String[] args) throws Exception {
-    
-        DatagramSocket serverSocket = new DatagramSocket(12345);
-        System.out.println("UDP Server is running...");
 
-        byte[] receiveData = new byte[1024];
-        byte[] sendData = new byte[1024];
+        // Register a shutdown hook to catch Ctrl+C (SIGINT)
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("\nCtrl+C detected. Stopping the main thread...");
+            running = false; // This will break the main loop
+        }));
 
-        while (true) {
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
+        // Listens for incoming client connections
+        ServerSocket welcomeSocket = new ServerSocket(PORT);
+        System.out.println("Listening on port " + PORT);
+        System.out.println("Waiting for connections...");
 
-            String sentence = new String(receivePacket.getData());
-            System.out.println("FROM CLIENT: " + sentence);
+        while (running) {
+            // When a client connects, this represents the connection for that client
+            // New socket created for each client
+            Socket connectionSocket = welcomeSocket.accept();
 
-            String modifiedSentence = sentence.toUpperCase();
-            sendData = modifiedSentence.getBytes();
+            // Create a new client object -> attach the socket to the client object
+            Client client = new Client(connectionSocket, clientList);
+            clientList.put(connectionSocket.toString(), client);
 
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
-            serverSocket.send(sendPacket);
+            Thread clientThread = new Thread(client);
+            //invokes run
+            clientThread.start();
+
+            int socketCount = numClient++;
+            System.out.println("New connection, thread name is " + clientThread.getName() +
+                                ", ip is: " + client.getAddress() + 
+                                ", port is: " + client.getPort());
+            System.out.println("Adding to list of sockets as " + socketCount);
+        }  
+         
+        welcomeSocket.close();
+    }
+}
+
+class Client implements Runnable {
+    private Socket clientSocket;
+    private BufferedReader inFromClient;
+    private DataOutputStream outToClient;
+    private ConcurrentHashMap<String, Client> clientList;
+
+    public Client (Socket socket, ConcurrentHashMap<String, Client> clientList) {
+        this.clientSocket = socket;
+        this.clientList = clientList;
+    }
+
+    @Override
+    public void run() {
+        try {
+            inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToClient = new DataOutputStream(clientSocket.getOutputStream());
+            
+            String clientMessage;
+            while (tcpcss.running) {
+                clientMessage = inFromClient.readLine();
+
+                // if client disconnects (it becomes null)
+                if (clientMessage == null) {
+                    break;
+                }
+                System.out.println("client: " + clientMessage);
+                sendToAll(clientMessage);
+            }
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            disconnectCleanup();
         }
+    }
+
+    public void sendToAll(String message) {
+        // for each client in the list, if the values of the client...
+        for (Client client : clientList.values()) {
+            if (client != this) { // is not the same - send to the other clients
+                try {
+                    client.outToClient.writeBytes(message + "\n");
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+
+    private void disconnectCleanup() {
+        try {
+            clientSocket.close();
+            System.out.println("Host disconnected, ip " + getAddress() +
+                                ", port " + getPort());
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            clientList.remove(clientSocket.toString());
+        }
+    }
+
+    public String getAddress() {
+        return clientSocket.getInetAddress().getHostAddress();
+    }
+
+    public int getPort() {
+        return clientSocket.getPort();
     }
 }
